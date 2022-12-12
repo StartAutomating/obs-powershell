f93833faac68df1287915e8ffc1348a4737f2d28
James Brundage <@github.com>
2022-12-11 22:21:06 -0800

    Adding obs-powershell tests

--- obs-powershell.tests.ps1
+++ obs-powershell.tests.ps1
@@ -2,6 +2,13 @@ describe "obs-powershell" {
     it "Lets you control OBS from PowerShell" {
         Get-OBSMonitor -PassThru | 
             Select-Object -ExpandProperty requestID |
-            Should -BeLike GetMonitorList
-    }    
+            Should -BeLike GetMonitorList*
+    }
+    
+    it 'Has lots of OBS websocket commands' {
+        Get-Command -Name *-OBS | 
+            Measure-Object |
+            Select-Object -ExpandProperty Count |
+            Should -BeGreaterThan 100
+    }
 }

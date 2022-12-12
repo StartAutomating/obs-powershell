491f9b2ea6c5868b247747192075576c9da1656c
James Brundage <@github.com>
2022-12-11 22:33:10 -0800

    Updating obs-powershell tests

--- obs-powershell.tests.ps1
+++ obs-powershell.tests.ps1
@@ -11,4 +11,27 @@ describe "obs-powershell" {
             Select-Object -ExpandProperty Count |
             Should -BeGreaterThan 100
     }
+
+    it 'Communicates requests in a predictable way' {
+        $allRequestCommands = Get-Command -Name *-OBS* |
+            Where-Object { 
+                $_.ScriptBlock.Attributes.Key -contains 'OBS.WebSocket.RequestType'
+            }
+
+        $allEasyCommands = $allRequestCommands | 
+            Where-Object {
+                ($_ | Get-Command -Syntax) -match "$($_.Name) \[-PassThru\]"
+            }
+
+        foreach ($command in $allEasyCommands) {
+            $requestType  = foreach ($attr in $command.ScriptBlock.Attributes) {
+                if ($attr.Key -eq 'OBS.WebSocket.RequestType') {
+                    $attr.Value;break
+                }
+            }
+            $cmdOut = & $command -PassThru
+            $cmdOut | Select-Object -ExpandProperty RequestID | Should -belike "$requestType*"
+        }
+        
+    }
 }

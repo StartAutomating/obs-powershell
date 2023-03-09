6d6f23f4cc9e9192aa858460ca923e8d1b727225
StartAutomating <StartAutomating@github.com>
2023-03-09 03:04:22 +0000

    Adding -Force to Add-OBS*Source commands (Fixes #69)

--- Commands/Sources/Add-OBSBrowserSource.ps1
+++ Commands/Sources/Add-OBSBrowserSource.ps1
@@ -64,7 +64,12 @@ function Add-OBSBrowserSource {
     # If no name is provided, the last segment of the URI or file path will be the input name.
     [Parameter(ValueFromPipelineByPropertyName)]
     [string]
-    $Name
+    $Name,
+# If set, will check if the source exists in the scene before creating it and removing any existing sources found.
+    # If not set, you will get an error if a source with the same name exists.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [switch]
+    $Force
     )
 dynamicParam {
     $baseCommand = 
@@ -159,6 +164,13 @@ dynamicParam {
                 } else {
                     $uri
                 }
+        }
+        # If -Force is provided
+        if ($Force) {
+            # Clear any items from that scene
+            Get-OBSSceneItem -sceneName $myParameters["Scene"] |
+                Where-Object SourceName -eq $name |
+                Remove-OBSInput -InputName { $_.SourceName }
         }
         $outputAddedResult = 
             Add-OBSInput -sceneName $myParameters["Scene"] -inputKind "browser_source" -inputSettings $myParameterData -inputName $Name

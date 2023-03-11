4ae733df94fd964fabc47896de1fe0de347ed213
StartAutomating <StartAutomating@github.com>
2023-03-11 01:25:15 +0000

    Add-OBS*Source Commands:  Supporting -SceneItemEnabled (Fixes #70)

--- Commands/Sources/Add-OBSBrowserSource.ps1
+++ Commands/Sources/Add-OBSBrowserSource.ps1
@@ -172,8 +172,20 @@ dynamicParam {
                 Where-Object SourceName -eq $name |
                 Remove-OBSInput -InputName { $_.SourceName }
         }
+        $addObsInputParams = [Ordered]@{
+            sceneName = $myParameters["Scene"]
+            inputKind = "browser_source"
+            inputSettings = $myParameterData
+            inputName = $Name
+        }
+        # If -SceneItemEnabled was passed,
+        if ($myParameters.Contains('SceneItemEnabled')) {
+            # propagate it to Add-OBSInput.
+            $addObsInputParams.SceneItemEnabled = $myParameters['SceneItemEnabled'] -as [bool]
+        }
+        
         $outputAddedResult = 
-            Add-OBSInput -sceneName $myParameters["Scene"] -inputKind "browser_source" -inputSettings $myParameterData -inputName $Name
+            Add-OBSInput @addObsInputParams
         if ($outputAddedResult) {
             Get-OBSSceneItem -sceneName $myParameters["Scene"] |
                 Where-Object SourceName -eq $name

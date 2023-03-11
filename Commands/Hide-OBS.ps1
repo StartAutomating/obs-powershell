9650032ce16019f3d728826dce285378be9a5941
James Brundage <@github.com>
2023-03-10 17:02:13 -0800

    Adding Hide-OBS (Fixes #67)

---
+++ Commands/Hide-OBS.ps1
@@ -0,0 +1,43 @@
+function Hide-OBS {
+    <#
+    .SYNOPSIS
+        Hide OBS
+    .DESCRIPTION
+        Hides items in OBS
+    .EXAMPLE
+        Hide-OBS -SourceName "foo"        
+    .LINK
+        Set-OBSSceneItemEnabled
+    #>
+    [CmdletBinding(SupportsShouldProcess,ConfirmImpact='Medium')]
+    param(
+    # The name of the item we want to Hide
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+    [Alias('SourceName','InputName','SceneName')]
+    $ItemName,
+
+    # The name of the scene.  If not provided, the current program scene will be used.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $SceneName
+    )
+
+    process {
+        # If no scene was provided
+        if (-not $SceneName) {
+            # find the current program scene
+            $sceneName = Get-OBSCurrentProgramScene
+        }
+
+        # Walk over all items in the scene
+        foreach ($sceneItem in Get-OBSSceneItem -SceneName $SceneName) {
+            # If the match our wildcard and we confirm, remove it.
+            if ($sceneItem.SourceName -like $sourceName -and
+                $PSCmdlet.ShouldProcess("Hide input $($sceneItem.SourceName)")) {
+                # Hide it.
+                $sceneItem | Set-OBSSceneItemEnabled -SceneItemEnabled:$false
+            }            
+        }
+    }
+}
+

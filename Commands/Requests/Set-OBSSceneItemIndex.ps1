3671f6efa90734df7f5ba837370cab8c3e4280e7
StartAutomating <StartAutomating@github.com>
2023-04-08 00:08:38 +0000

    obs scene items
    Making Animate more careful (Fixes #73)

--- Commands/Requests/Set-OBSSceneItemIndex.ps1
+++ Commands/Requests/Set-OBSSceneItemIndex.ps1
@@ -39,22 +39,16 @@ $SceneItemIndex,
 [switch]
 $PassThru
 )
-
-
 process {
-
-
         # Create a copy of the parameters (that are part of the payload)
         $paramCopy = [Ordered]@{}
         # get a reference to this command
         $myCmd = $MyInvocation.MyCommand
-
         # Keep track of how many requests we have done of a given type
         # (this makes creating RequestIDs easy)
         if (-not $script:ObsRequestsCounts) {
             $script:ObsRequestsCounts = @{}
         }
-
         # Set my requestType to blank
         $myRequestType = ''
         # and indicate we are not expecting a response
@@ -74,7 +68,6 @@ process {
                 }
             }
         }
-
         # Walk over each parameter
         :nextParam foreach ($keyValue in $PSBoundParameters.GetEnumerator()) {
             # and walk over each of it's attributes to see if it part of the payload
@@ -108,15 +101,12 @@ process {
             # and optional data
             requestData = $paramCopy
         }
-
         if ($PassThru) {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
                 Send-OBS
         }
-
 }
-
 } 
 

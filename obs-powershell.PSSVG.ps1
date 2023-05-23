7d6ceefbbb9df5b0cbc56eb8d44542f13eed8377
James Brundage <@github.com>
2023-05-23 13:44:02 -0700

    Adding icon variations of logo (re #76)

--- obs-powershell.PSSVG.ps1
+++ obs-powershell.PSSVG.ps1
@@ -31,3 +31,25 @@ svg @(
     )
     svg.text -Content "obs-powershell" -Fill '#4488ff' -TextAnchor 'middle' -X 50% -y 65% -FontSize 72 -AlignmentBaseline 'middle' -FontFamily 'sans-serif'
 ) -ViewBox 1000,1000 -OutputPath (Join-Path $assetsPath obs-powershell.svg)
+
+foreach ($variant in 'icon','icon-animated') {
+
+    svg @(
+        svg.symbol -ViewBox $powerShellChevron.svg.viewBox -Content $powerShellChevron.svg.symbol.InnerXml -Id psChevron
+        
+        SVG.mask -Id "psChevronMask" -Content @(
+            SVG.rect -X 150 -Y 150 -Height 200 -Width 200 -Fill white
+            SVG.circle -Cx 250 -Cy 250 -R 90 -Fill black
+        
+            svg.use -Href '#psChevron' -Fill 'white' -Height 75 -Y 35% -X 0 -Transform "rotate(0, 250, 250)" 
+            svg.use -Href '#psChevron' -Fill 'white' -Height 75 -Y 35% -X 0 -Transform "rotate(120, 250, 250)"
+            svg.use -Href '#psChevron' -Fill 'white' -Height 75 -Y 35% -X 0 -Transform "rotate(240, 250, 250)"        
+        ) 
+        
+        SVG.circle -Fill '#4488ff' -Cx 250 -Cy 250 -R 100  -Mask 'url(#psChevronMask)' @(
+            if ($variant -like '*animated*') {
+                svg.animatetransform -AttributeName transform -From "0 250 250"  -To "360 250 250" -dur "5s" -RepeatCount indefinite -AttributeType xml -type rotate
+            }
+        )
+    ) -ViewBox 500,500 -OutputPath (Join-Path $assetsPath "obs-powershell-$variant.svg")
+}

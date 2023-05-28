773cf43310818b22284cda642dee5c1fa86cf4d7
James Brundage <@github.com>
2023-05-27 19:01:19 -0700

    Adding Set-OBSGainFilter (Fixes #94)

---
+++ Commands/Filters/Set-OBSGainFilter.ps.ps1
@@ -0,0 +1,114 @@
+function Set-OBSGainFilter
+{
+    <#
+    .SYNOPSIS
+        Sets a Gain filter.
+    .DESCRIPTION
+        Adds or Changes a Gain Filter on an OBS Input.
+
+        This allows you to make the audio louder or softer.
+    .EXAMPLE
+        Show-OBS -Uri https://pssvg.start-automating.com/Examples/Stars.svg |
+            Set-OBSGainFilter -HorizontalSpeed 100 -VerticalSpeed 100
+    #>
+    [inherit(Command={
+        Import-Module ..\..\obs-powershell.psd1 -Global
+        "Add-OBSSourceFilter"
+    }, Dynamic, Abstract, ExcludeParameter='FilterKind','FilterName','FilterSettings')]
+    [Alias('Add-OBSGainFilter')]    
+    param(
+    # The horizontal Gain speed.
+    [Parameter(ValueFromPipelineByPropertyName)]    
+    [ComponentModel.DefaultBindingProperty("db")]    
+    [double]
+    $Gain,
+    
+    # The name of the filter.  It will default to "ColorCorrection".
+    [Parameter(ValueFromPipelineByPropertyName)]    
+    [string]
+    $FilterName,
+
+    # If set, will remove a filter if one already exists.
+    # If this is not provided and the filter already exists, the settings of the filter will be changed.
+    [switch]
+    $Force
+    )
+    
+    process {
+        $myParameters = [Ordered]@{} + $PSBoundParameters
+        
+        if (-not $myParameters["FilterName"]) {
+            $filterName = $myParameters["FilterName"] = "Gain"
+        }
+                
+        $myParameterData = [Ordered]@{}
+        foreach ($parameter in $MyInvocation.MyCommand.Parameters.Values) {
+
+            $bindToPropertyName = $null            
+            
+            foreach ($attribute in $parameter.Attributes) {
+                if ($attribute -is [ComponentModel.DefaultBindingPropertyAttribute]) {
+                    $bindToPropertyName = $attribute.Name
+                    break
+                }
+            }
+
+            if (-not $bindToPropertyName) { continue }
+            if ($myParameters.Contains($parameter.Name)) {
+                $myParameterData[$bindToPropertyName] = $myParameters[$parameter.Name]
+                if ($myParameters[$parameter.Name] -is [switch]) {
+                    $myParameterData[$bindToPropertyName] = $parameter.Name -as [bool]
+                }
+            }
+        }        
+        
+        $addSplat = @{            
+            filterName = $myParameters["FilterName"]
+            SourceName = $myParameters["SourceName"]
+            filterKind = "gain_filter"
+            filterSettings = $myParameterData
+        }        
+
+        # If -SceneItemEnabled was passed,
+        if ($myParameters.Contains('SceneItemEnabled')) {
+            # propagate it to Add-OBSInput.
+            $addSplat.SceneItemEnabled = $myParameters['SceneItemEnabled'] -as [bool]
+        }
+
+        # Add the input.
+        $outputAddedResult = Add-OBSSourceFilter @addSplat *>&1
+
+        # If we got back an error
+        if ($outputAddedResult -is [Management.Automation.ErrorRecord]) {
+            # and that error was saying the source already exists, 
+            if ($outputAddedResult.TargetObject.d.requestStatus.code -eq 601) {
+                # then check if we use the -Force.
+                if ($Force)  { # If we do, remove the input                    
+                    Remove-OBSSourceFilter -FilterName $addSplat.FilterName -SourceName $addSplat.SourceName
+                    # and re-add our result.
+                    $outputAddedResult = Add-OBSInput @addSplat *>&1
+                } else {
+                    # Otherwise, get the input from the filters.
+                    $existingFilter = Get-OBSSourceFilter -SourceName $addSplat.SourceName -FilterName $addSplat.FilterName 
+                    $existingFilter.Set($addSplat.filterSettings)
+                    $existingFilter
+                    $outputAddedResult = $null
+                }
+            }
+
+            # If the output was still an error
+            if ($outputAddedResult -is [Management.Automation.ErrorRecord]) {
+                # use $psCmdlet.WriteError so that it shows the error correctly.
+                $psCmdlet.WriteError($outputAddedResult)
+            }
+            
+        }
+        # Otherwise, if we had a result
+        elseif ($outputAddedResult) {
+            # Otherwise, get the input from the filters.
+            Get-OBSSourceFilter -SourceName $addSplat.SourceName -FilterName $addSplat.FilterName
+                
+        }
+    }
+}
+

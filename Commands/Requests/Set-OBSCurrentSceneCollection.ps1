553327414bd5b6786f011b2d63bbd81236c20cc4
StartAutomating <StartAutomating@github.com>
2023-05-31 02:47:42 +0000

    Not requesting a response when none is required (re #107)

--- Commands/Requests/Set-OBSCurrentSceneCollection.ps1
+++ Commands/Requests/Set-OBSCurrentSceneCollection.ps1
@@ -94,7 +94,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS -DoNotReceive:$responseExpected
+                Send-OBS -NoResponse:(-not $responseExpected)
         }
 }
 } 

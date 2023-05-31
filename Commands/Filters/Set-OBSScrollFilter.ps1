5059670c087c4f9212acc3197c431e19afb16caf
StartAutomating <StartAutomating@github.com>
2023-05-31 03:54:52 +0000

    OBS.GetSceneItemList - adding .Zindex/SetZindex
    Also, updating Animate to allow for pauses

--- Commands/Filters/Set-OBSScrollFilter.ps1
+++ Commands/Filters/Set-OBSScrollFilter.ps1
@@ -60,7 +60,7 @@ function Set-OBSScrollFilter {
             $script:AddOBSSourceFilter
         }
     $IncludeParameter = @()
-    $ExcludeParameter = 'FilterKind','FilterName','FilterSettings'
+    $ExcludeParameter = 'FilterKind','FilterSettings'
     $DynamicParameters = [Management.Automation.RuntimeDefinedParameterDictionary]::new()            
     :nextInputParameter foreach ($paramName in ([Management.Automation.CommandMetaData]$baseCommand).Parameters.Keys) {
         if ($ExcludeParameter) {

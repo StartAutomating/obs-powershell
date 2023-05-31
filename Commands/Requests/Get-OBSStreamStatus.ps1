15770856f922135f6cdb3f260a34e0a8e111c317
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:32 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Get-OBSStreamStatus.ps1
+++ Commands/Requests/Get-OBSStreamStatus.ps1
@@ -15,6 +15,7 @@ function Get-OBSStreamStatus {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#getstreamstatus
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'GetStreamStatus')]
+[Alias('obs.powershell.websocket.GetStreamStatus')]
 [Reflection.AssemblyMetadata('OBS.WebSocket.ExpectingResponse', $true)]
 param(
 # If set, will return the information that would otherwise be sent to OBS.
@@ -89,7 +90,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

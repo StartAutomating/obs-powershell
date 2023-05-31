73559ac5ca60a55a1f66ca87137c97deb2806b9e
StartAutomating <StartAutomating@github.com>
2023-05-31 02:47:41 +0000

    Not requesting a response when none is required (re #107)

--- Commands/Requests/Open-OBSVideoMixProjector.ps1
+++ Commands/Requests/Open-OBSVideoMixProjector.ps1
@@ -110,7 +110,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS -DoNotReceive:$responseExpected
+                Send-OBS -NoResponse:(-not $responseExpected)
         }
 }
 } 

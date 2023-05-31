4ccbbe2caba1a2457c6d65b508ffed41c965e41e
StartAutomating <StartAutomating@github.com>
2023-05-31 02:47:44 +0000

    Not requesting a response when none is required (re #107)

--- Commands/Requests/Set-OBSSourceFilterEnabled.ps1
+++ Commands/Requests/Set-OBSSourceFilterEnabled.ps1
@@ -102,7 +102,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS -DoNotReceive:$responseExpected
+                Send-OBS -NoResponse:(-not $responseExpected)
         }
 }
 } 

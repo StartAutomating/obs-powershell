f519981b1e9b270f03fd83c5654dcfcb235bd267
StartAutomating <StartAutomating@github.com>
2023-05-31 02:47:40 +0000

    Not requesting a response when none is required (re #107)

--- Commands/Requests/Get-OBSSourceScreenshot.ps1
+++ Commands/Requests/Get-OBSSourceScreenshot.ps1
@@ -121,7 +121,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS -DoNotReceive:$responseExpected
+                Send-OBS -NoResponse:(-not $responseExpected)
         }
 }
 } 

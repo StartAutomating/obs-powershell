7d2f06a85da4ab63dc241db2864b92b59952bdd2
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:30 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Get-OBSHotkey.ps1
+++ Commands/Requests/Get-OBSHotkey.ps1
@@ -15,6 +15,7 @@ function Get-OBSHotkey {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#gethotkeylist
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'GetHotkeyList')]
+[Alias('obs.powershell.websocket.GetHotkeyList')]
 [Reflection.AssemblyMetadata('OBS.WebSocket.ExpectingResponse', $true)]
 param(
 # If set, will return the information that would otherwise be sent to OBS.
@@ -89,7 +90,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

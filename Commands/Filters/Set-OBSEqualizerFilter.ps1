83f32c20967b0b50074ffe2b4472fe3f98e2ffb6
StartAutomating <StartAutomating@github.com>
2023-05-31 03:54:52 +0000

    OBS.GetSceneItemList - adding .Zindex/SetZindex
    Also, updating Animate to allow for pauses

--- Commands/Filters/Set-OBSEqualizerFilter.ps1
+++ Commands/Filters/Set-OBSEqualizerFilter.ps1
@@ -44,7 +44,7 @@ function Set-OBSEqualizerFilter {
             $script:AddOBSSourceFilter
         }
     $IncludeParameter = @()
-    $ExcludeParameter = 'FilterKind','FilterName','FilterSettings'
+    $ExcludeParameter = 'FilterKind','FilterSettings'
     $DynamicParameters = [Management.Automation.RuntimeDefinedParameterDictionary]::new()            
     :nextInputParameter foreach ($paramName in ([Management.Automation.CommandMetaData]$baseCommand).Parameters.Keys) {
         if ($ExcludeParameter) {

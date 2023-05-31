8677e3e9f78bc80909329daa027771a77877b7b2
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:35 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Set-OBSSceneItemEnabled.ps1
+++ Commands/Requests/Set-OBSSceneItemEnabled.ps1
@@ -15,6 +15,7 @@ function Set-OBSSceneItemEnabled {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#setsceneitemenabled
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'SetSceneItemEnabled')]
+[Alias('obs.powershell.websocket.SetSceneItemEnabled')]
 param(
 <# Name of the scene the item is in #>
 [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
@@ -104,7 +105,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

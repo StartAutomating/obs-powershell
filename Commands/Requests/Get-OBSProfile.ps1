c92661d6fd3ab1a75a79531985e90e2b84acfb09
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:31 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Get-OBSProfile.ps1
+++ Commands/Requests/Get-OBSProfile.ps1
@@ -15,6 +15,7 @@ function Get-OBSProfile {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#getprofilelist
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'GetProfileList')]
+[Alias('obs.powershell.websocket.GetProfileList')]
 [Reflection.AssemblyMetadata('OBS.WebSocket.ExpectingResponse', $true)]
 param(
 # If set, will return the information that would otherwise be sent to OBS.
@@ -89,7 +90,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

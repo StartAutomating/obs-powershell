7f405dac9f0c239805e760a9ea7b3601218cb758
StartAutomating <StartAutomating@github.com>
2023-05-31 02:47:43 +0000

    Not requesting a response when none is required (re #107)

--- Commands/Requests/Set-OBSOutputSettings.ps1
+++ Commands/Requests/Set-OBSOutputSettings.ps1
@@ -97,7 +97,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS -DoNotReceive:$responseExpected
+                Send-OBS -NoResponse:(-not $responseExpected)
         }
 }
 } 

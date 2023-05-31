0e731e393eb9edcb610c0e8580b86712bda843aa
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:31 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Get-OBSSceneCollection.ps1
+++ Commands/Requests/Get-OBSSceneCollection.ps1
@@ -15,6 +15,7 @@ function Get-OBSSceneCollection {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#getscenecollectionlist
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'GetSceneCollectionList')]
+[Alias('obs.powershell.websocket.GetSceneCollectionList')]
 [Reflection.AssemblyMetadata('OBS.WebSocket.ExpectingResponse', $true)]
 param(
 # If set, will return the information that would otherwise be sent to OBS.
@@ -89,7 +90,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

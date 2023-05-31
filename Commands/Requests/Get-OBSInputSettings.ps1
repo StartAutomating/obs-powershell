b787c5dbf417a6b28fc232910e428aa7e1a9f748
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:30 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Get-OBSInputSettings.ps1
+++ Commands/Requests/Get-OBSInputSettings.ps1
@@ -15,6 +15,7 @@ function Get-OBSInputSettings {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#getinputsettings
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'GetInputSettings')]
+[Alias('obs.powershell.websocket.GetInputSettings')]
 [Reflection.AssemblyMetadata('OBS.WebSocket.ExpectingResponse', $true)]
 param(
 <# Name of the input to get the settings of #>
@@ -94,7 +95,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

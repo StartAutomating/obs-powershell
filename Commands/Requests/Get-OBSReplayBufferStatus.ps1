ad8fbc757a6a69774f98c9ff7f3a2dbd7e2eea82
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:31 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Get-OBSReplayBufferStatus.ps1
+++ Commands/Requests/Get-OBSReplayBufferStatus.ps1
@@ -15,6 +15,7 @@ function Get-OBSReplayBufferStatus {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#getreplaybufferstatus
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'GetReplayBufferStatus')]
+[Alias('obs.powershell.websocket.GetReplayBufferStatus')]
 [Reflection.AssemblyMetadata('OBS.WebSocket.ExpectingResponse', $true)]
 param(
 # If set, will return the information that would otherwise be sent to OBS.
@@ -89,7 +90,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

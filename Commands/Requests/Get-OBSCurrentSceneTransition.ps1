60166553a889d0e8858a5df4df3dfbfd81a75441
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:29 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Get-OBSCurrentSceneTransition.ps1
+++ Commands/Requests/Get-OBSCurrentSceneTransition.ps1
@@ -15,6 +15,7 @@ function Get-OBSCurrentSceneTransition {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#getcurrentscenetransition
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'GetCurrentSceneTransition')]
+[Alias('obs.powershell.websocket.GetCurrentSceneTransition')]
 [Reflection.AssemblyMetadata('OBS.WebSocket.ExpectingResponse', $true)]
 param(
 # If set, will return the information that would otherwise be sent to OBS.
@@ -89,7 +90,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

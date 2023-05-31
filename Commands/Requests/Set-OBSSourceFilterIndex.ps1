916fc27541d59e9ffb39f6be5c8efc91d72ede3f
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:36 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Set-OBSSourceFilterIndex.ps1
+++ Commands/Requests/Set-OBSSourceFilterIndex.ps1
@@ -13,6 +13,7 @@ function Set-OBSSourceFilterIndex {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#setsourcefilterindex
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'SetSourceFilterIndex')]
+[Alias('obs.powershell.websocket.SetSourceFilterIndex')]
 param(
 <# Name of the source the filter is on #>
 [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
@@ -102,7 +103,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

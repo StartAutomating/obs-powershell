4cf56b150b75fd32ab66b0b3b0a42fe6bb6f6183
StartAutomating <StartAutomating@github.com>
2023-05-31 02:19:36 +0000

    Aliasing Request Commands to obs.powershell.websocket.$RequestType (Fixes #108)
    Also, not requesting responses that will never come (re #107)

--- Commands/Requests/Stop-OBSRecord.ps1
+++ Commands/Requests/Stop-OBSRecord.ps1
@@ -15,6 +15,7 @@ function Stop-OBSRecord {
     https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#stoprecord
 #>
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'StopRecord')]
+[Alias('obs.powershell.websocket.StopRecord')]
 [Reflection.AssemblyMetadata('OBS.WebSocket.ExpectingResponse', $true)]
 param(
 # If set, will return the information that would otherwise be sent to OBS.
@@ -89,7 +90,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS
+                Send-OBS -DoNotReceive:$responseExpected
         }
 }
 } 

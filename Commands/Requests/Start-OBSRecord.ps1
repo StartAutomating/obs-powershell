57feabfe8eef93312d27c584676bbc30cd1f7639
StartAutomating <StartAutomating@github.com>
2023-05-31 02:47:44 +0000

    Not requesting a response when none is required (re #107)

--- Commands/Requests/Start-OBSRecord.ps1
+++ Commands/Requests/Start-OBSRecord.ps1
@@ -89,7 +89,7 @@ process {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
-                Send-OBS -DoNotReceive:$responseExpected
+                Send-OBS -NoResponse:(-not $responseExpected)
         }
 }
 } 

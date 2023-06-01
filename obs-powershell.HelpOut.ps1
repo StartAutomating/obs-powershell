0a100dffbaec253f14e420b19df7f8a89a8a3385
James Brundage <@github.com>
2023-06-01 14:07:29 -0700

    Auto-generating help for effect commands (re #109, #112)

--- obs-powershell.HelpOut.ps1
+++ obs-powershell.HelpOut.ps1
@@ -1,5 +1,5 @@
 #requires -Module HelpOut
 
 Import-Module .\obs-powershell.psd1
-Save-MarkdownHelp -Module obs-powershell -PassThru
+Save-MarkdownHelp -Module obs-powershell -PassThru -Command (Get-OBSEffect | Where-Object { $_ -is [Management.Automation.CommandInfo]})
 

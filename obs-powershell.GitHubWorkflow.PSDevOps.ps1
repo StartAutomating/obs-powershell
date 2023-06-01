6e33987abe79548f13aa28855a08c5337d6e9ac0
James Brundage <@github.com>
2023-06-01 16:02:37 -0700

    Updating Workflow (removing OnIssue, adding GitLogger)

--- obs-powershell.GitHubWorkflow.PSDevOps.ps1
+++ obs-powershell.GitHubWorkflow.PSDevOps.ps1
@@ -1,7 +1,6 @@
 
 #requires -Module PSDevOps
 #requires -Module obs-powershell
-#requires -Module GitPub
 
 Import-BuildStep -Module obs-powershell
 
@@ -10,12 +9,6 @@ Push-Location $PSScriptRoot
 New-GitHubWorkflow -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, BuildOBSPowerShell -OutputPath @'
 .\.github\workflows\build-obs-powershell.yml
 '@ -Name "Build, Test, and Release obs-powershell" -On Push, PullRequest
-
-Import-BuildStep -ModuleName GitPub
-
-New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name GitPub -OutputPath @'
-.\.github\workflows\GitPub.yml
-'@
 
 Pop-Location
 

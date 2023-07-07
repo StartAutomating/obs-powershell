bb70543006794f8664a5d30be4eedc9540ec6777
StartAutomating <StartAutomating@github.com>
2023-07-07 18:32:19 +0000

    OBS Filter Commands - Fixing -NoResponse (Fixes #136)

--- Commands/Filters/Set-OBSColorFilter.ps1
+++ Commands/Filters/Set-OBSColorFilter.ps1
@@ -163,6 +163,7 @@ function Set-OBSColorFilter {
             SourceName = $myParameters["SourceName"]
             filterKind = "color_filter_v2"
             filterSettings = $myParameterData
+            NoResponse = $myParameters["NoResponse"]
         }
         
         if ($MyParameters["PassThru"]) {

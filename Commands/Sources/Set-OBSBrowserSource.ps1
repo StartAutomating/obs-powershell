8364c66c3bc0c8e783d680b0d684bc2c43127f3e
StartAutomating <StartAutomating@github.com>
2023-07-07 18:42:47 +0000

    OBS Source Commands - Fixing -NoResponse (Fixes #136)
    Also fixing Add vs Set return behaviors

--- Commands/Sources/Set-OBSBrowserSource.ps1
+++ Commands/Sources/Set-OBSBrowserSource.ps1
@@ -174,6 +174,7 @@ function Set-OBSBrowserSource {
             inputKind = "browser_source"
             inputSettings = $myParameterData
             inputName = $Name
+            NoResponse = $myParameters["NoResponse"]
         }
         # If -SceneItemEnabled was passed,
         if ($myParameters.Contains('SceneItemEnabled')) {
@@ -226,7 +227,8 @@ function Set-OBSBrowserSource {
             }            
         }
         # Otherwise, if we had a result
-        elseif ($outputAddedResult) {
+        if ($outputAddedResult -and 
+            $outputAddedResult -isnot [Management.Automation.ErrorRecord]) {
             # get the input from the scene.
             Get-OBSSceneItem -sceneName $myParameters["Scene"] |
                 Where-Object SourceName -eq $myParameters["Name"]

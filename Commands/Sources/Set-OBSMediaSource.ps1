e1b9464bb1e321ae1879c5a83193a170de480a28
StartAutomating <StartAutomating@github.com>
2023-07-07 18:42:47 +0000

    OBS Source Commands - Fixing -NoResponse (Fixes #136)
    Also fixing Add vs Set return behaviors

--- Commands/Sources/Set-OBSMediaSource.ps1
+++ Commands/Sources/Set-OBSMediaSource.ps1
@@ -172,6 +172,7 @@ function Set-OBSMediaSource {
             inputKind = "ffmpeg_source"
             inputSettings = $myParameterData
             inputName = $Name
+            NoResponse = $myParameters["NoResponse"]
         }
         if ($myParameters.Contains('SceneItemEnabled')) {
             $addSplat.SceneItemEnabled = $myParameters['SceneItemEnabled'] -as [bool]
@@ -220,8 +221,9 @@ function Set-OBSMediaSource {
                 $psCmdlet.WriteError($outputAddedResult)
             }
         }
+        
         # Otherwise, if we had a result
-        elseif ($outputAddedResult) {
+        if ($outputAddedResult -isnot [Management.Automation.ErrorRecord]) {
             # get the input from the scene and optionally fit it to the screen.
             Get-OBSSceneItem -sceneName $myParameters["Scene"] |
                 Where-Object SourceName -eq $name |

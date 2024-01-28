fa9912a6f3d1d937b7c1d8deaaedc0c0140f6530
StartAutomating <StartAutomating@github.com>
2024-01-28 20:27:56 +0000

    feat: Regenerating with serial build

--- Commands/Requests/Set-OBSCurrentPreviewScene.ps1
+++ Commands/Requests/Set-OBSCurrentPreviewScene.ps1
@@ -1,4 +1,5 @@
 function Set-OBSCurrentPreviewScene {
+
 <#
 .Synopsis
     
@@ -17,11 +18,16 @@ function Set-OBSCurrentPreviewScene {
 [Reflection.AssemblyMetadata('OBS.WebSocket.RequestType', 'SetCurrentPreviewScene')]
 [Alias('obs.powershell.websocket.SetCurrentPreviewScene')]
 param(
-<# Scene to set as the current preview scene #>
-[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+<# Scene name to set as the current preview scene #>
+[Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('sceneName')]
 [string]
 $SceneName,
+<# Scene UUID to set as the current preview scene #>
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('sceneUuid')]
+[string]
+$SceneUuid,
 # If set, will return the information that would otherwise be sent to OBS.
 [Parameter(ValueFromPipelineByPropertyName)]
 [Alias('OutputRequest','OutputInput')]
@@ -34,16 +40,22 @@ $PassThru,
 [switch]
 $NoResponse
 )
+
+
 process {
+
+
         # Create a copy of the parameters (that are part of the payload)
         $paramCopy = [Ordered]@{}
         # get a reference to this command
         $myCmd = $MyInvocation.MyCommand
+
         # Keep track of how many requests we have done of a given type
         # (this makes creating RequestIDs easy)
         if (-not $script:ObsRequestsCounts) {
             $script:ObsRequestsCounts = @{}
         }
+
         # Set my requestType to blank
         $myRequestType = ''
         # and indicate we are not expecting a response
@@ -63,6 +75,7 @@ process {
                 }
             }
         }
+
         # Walk over each parameter
         :nextParam foreach ($keyValue in $PSBoundParameters.GetEnumerator()) {
             # and walk over each of it's attributes to see if it part of the payload
@@ -96,12 +109,16 @@ process {
             # and optional data
             requestData = $paramCopy
         }
+
         if ($PassThru) {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
                 Send-OBS -NoResponse:$NoResponse
         }
+
 }
+
+
 } 
 

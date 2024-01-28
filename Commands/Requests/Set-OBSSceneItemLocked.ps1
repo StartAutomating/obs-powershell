fab393273cb4c21470b204bbbcd52b8e761aa879
StartAutomating <StartAutomating@github.com>
2024-01-28 20:27:57 +0000

    feat: Regenerating with serial build

--- Commands/Requests/Set-OBSSceneItemLocked.ps1
+++ Commands/Requests/Set-OBSSceneItemLocked.ps1
@@ -1,4 +1,5 @@
 function Set-OBSSceneItemLocked {
+
 <#
 .Synopsis
     
@@ -18,10 +19,15 @@ function Set-OBSSceneItemLocked {
 [Alias('obs.powershell.websocket.SetSceneItemLocked')]
 param(
 <# Name of the scene the item is in #>
-[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('sceneName')]
 [string]
 $SceneName,
+<# UUID of the scene the item is in #>
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('sceneUuid')]
+[string]
+$SceneUuid,
 <# Numeric ID of the scene item #>
 [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('sceneItemId')]
@@ -45,16 +51,22 @@ $PassThru,
 [switch]
 $NoResponse
 )
+
+
 process {
+
+
         # Create a copy of the parameters (that are part of the payload)
         $paramCopy = [Ordered]@{}
         # get a reference to this command
         $myCmd = $MyInvocation.MyCommand
+
         # Keep track of how many requests we have done of a given type
         # (this makes creating RequestIDs easy)
         if (-not $script:ObsRequestsCounts) {
             $script:ObsRequestsCounts = @{}
         }
+
         # Set my requestType to blank
         $myRequestType = ''
         # and indicate we are not expecting a response
@@ -74,6 +86,7 @@ process {
                 }
             }
         }
+
         # Walk over each parameter
         :nextParam foreach ($keyValue in $PSBoundParameters.GetEnumerator()) {
             # and walk over each of it's attributes to see if it part of the payload
@@ -107,12 +120,16 @@ process {
             # and optional data
             requestData = $paramCopy
         }
+
         if ($PassThru) {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
                 Send-OBS -NoResponse:$NoResponse
         }
+
 }
+
+
 } 
 

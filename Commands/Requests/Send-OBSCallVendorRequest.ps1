b660f9f48f1b0387ac98ebefceef3d353871c197
StartAutomating <StartAutomating@github.com>
2024-01-28 20:27:55 +0000

    feat: Regenerating with serial build

--- Commands/Requests/Send-OBSCallVendorRequest.ps1
+++ Commands/Requests/Send-OBSCallVendorRequest.ps1
@@ -1,4 +1,5 @@
 function Send-OBSCallVendorRequest {
+
 <#
 .Synopsis
     
@@ -46,16 +47,22 @@ $PassThru,
 [switch]
 $NoResponse
 )
+
+
 process {
+
+
         # Create a copy of the parameters (that are part of the payload)
         $paramCopy = [Ordered]@{}
         # get a reference to this command
         $myCmd = $MyInvocation.MyCommand
+
         # Keep track of how many requests we have done of a given type
         # (this makes creating RequestIDs easy)
         if (-not $script:ObsRequestsCounts) {
             $script:ObsRequestsCounts = @{}
         }
+
         # Set my requestType to blank
         $myRequestType = ''
         # and indicate we are not expecting a response
@@ -75,6 +82,7 @@ process {
                 }
             }
         }
+
         # Walk over each parameter
         :nextParam foreach ($keyValue in $PSBoundParameters.GetEnumerator()) {
             # and walk over each of it's attributes to see if it part of the payload
@@ -108,12 +116,16 @@ process {
             # and optional data
             requestData = $paramCopy
         }
+
         if ($PassThru) {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
                 Send-OBS -NoResponse:$NoResponse
         }
+
 }
+
+
 } 
 

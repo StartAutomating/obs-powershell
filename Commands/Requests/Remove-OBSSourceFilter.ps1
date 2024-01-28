41de16fe26d3a4ac34f4ebe714c8d80103474ba9
StartAutomating <StartAutomating@github.com>
2024-01-28 20:27:55 +0000

    feat: Regenerating with serial build

--- Commands/Requests/Remove-OBSSourceFilter.ps1
+++ Commands/Requests/Remove-OBSSourceFilter.ps1
@@ -1,4 +1,5 @@
 function Remove-OBSSourceFilter {
+
 <#
 .Synopsis
     
@@ -16,10 +17,15 @@ function Remove-OBSSourceFilter {
 [Alias('obs.powershell.websocket.RemoveSourceFilter')]
 param(
 <# Name of the source the filter is on #>
-[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+[Parameter(ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('sourceName')]
 [string]
 $SourceName,
+<# UUID of the source the filter is on #>
+[Parameter(ValueFromPipelineByPropertyName)]
+[ComponentModel.DefaultBindingProperty('sourceUuid')]
+[string]
+$SourceUuid,
 <# Name of the filter to remove #>
 [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
 [ComponentModel.DefaultBindingProperty('filterName')]
@@ -37,16 +43,22 @@ $PassThru,
 [switch]
 $NoResponse
 )
+
+
 process {
+
+
         # Create a copy of the parameters (that are part of the payload)
         $paramCopy = [Ordered]@{}
         # get a reference to this command
         $myCmd = $MyInvocation.MyCommand
+
         # Keep track of how many requests we have done of a given type
         # (this makes creating RequestIDs easy)
         if (-not $script:ObsRequestsCounts) {
             $script:ObsRequestsCounts = @{}
         }
+
         # Set my requestType to blank
         $myRequestType = ''
         # and indicate we are not expecting a response
@@ -66,6 +78,7 @@ process {
                 }
             }
         }
+
         # Walk over each parameter
         :nextParam foreach ($keyValue in $PSBoundParameters.GetEnumerator()) {
             # and walk over each of it's attributes to see if it part of the payload
@@ -99,12 +112,16 @@ process {
             # and optional data
             requestData = $paramCopy
         }
+
         if ($PassThru) {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
                 Send-OBS -NoResponse:$NoResponse
         }
+
 }
+
+
 } 
 

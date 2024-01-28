efe4ea871091ce5ebe8e966d3a748b698f52b696
StartAutomating <StartAutomating@github.com>
2024-01-28 20:27:51 +0000

    feat: Regenerating with serial build

--- Commands/Requests/Get-OBSCurrentPreviewScene.ps1
+++ Commands/Requests/Get-OBSCurrentPreviewScene.ps1
@@ -1,4 +1,5 @@
 function Get-OBSCurrentPreviewScene {
+
 <#
 .Synopsis
     
@@ -8,6 +9,8 @@ function Get-OBSCurrentPreviewScene {
     Gets the current preview scene.
     
     Only available when studio mode is enabled.
+    
+    Note: This request is slated to have the `currentPreview`-prefixed fields removed from in an upcoming RPC version.
     
     
     Get-OBSCurrentPreviewScene calls the OBS WebSocket with a request of type GetCurrentPreviewScene.
@@ -32,16 +35,22 @@ $PassThru,
 [switch]
 $NoResponse
 )
+
+
 process {
+
+
         # Create a copy of the parameters (that are part of the payload)
         $paramCopy = [Ordered]@{}
         # get a reference to this command
         $myCmd = $MyInvocation.MyCommand
+
         # Keep track of how many requests we have done of a given type
         # (this makes creating RequestIDs easy)
         if (-not $script:ObsRequestsCounts) {
             $script:ObsRequestsCounts = @{}
         }
+
         # Set my requestType to blank
         $myRequestType = ''
         # and indicate we are not expecting a response
@@ -61,6 +70,7 @@ process {
                 }
             }
         }
+
         # Walk over each parameter
         :nextParam foreach ($keyValue in $PSBoundParameters.GetEnumerator()) {
             # and walk over each of it's attributes to see if it part of the payload
@@ -94,12 +104,16 @@ process {
             # and optional data
             requestData = $paramCopy
         }
+
         if ($PassThru) {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
                 Send-OBS -NoResponse:$NoResponse
         }
+
 }
+
+
 } 
 

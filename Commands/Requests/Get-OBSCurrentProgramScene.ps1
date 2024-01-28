877f4cbb1598d45e42553ef9ebcf36b5c5220e5b
StartAutomating <StartAutomating@github.com>
2024-01-28 20:27:51 +0000

    feat: Regenerating with serial build

--- Commands/Requests/Get-OBSCurrentProgramScene.ps1
+++ Commands/Requests/Get-OBSCurrentProgramScene.ps1
@@ -1,4 +1,5 @@
 function Get-OBSCurrentProgramScene {
+
 <#
 .Synopsis
     
@@ -6,6 +7,8 @@ function Get-OBSCurrentProgramScene {
     
 .Description
     Gets the current program scene.
+    
+    Note: This request is slated to have the `currentProgram`-prefixed fields removed from in an upcoming RPC version.
     
     
     Get-OBSCurrentProgramScene calls the OBS WebSocket with a request of type GetCurrentProgramScene.
@@ -30,16 +33,22 @@ $PassThru,
 [switch]
 $NoResponse
 )
+
+
 process {
+
+
         # Create a copy of the parameters (that are part of the payload)
         $paramCopy = [Ordered]@{}
         # get a reference to this command
         $myCmd = $MyInvocation.MyCommand
+
         # Keep track of how many requests we have done of a given type
         # (this makes creating RequestIDs easy)
         if (-not $script:ObsRequestsCounts) {
             $script:ObsRequestsCounts = @{}
         }
+
         # Set my requestType to blank
         $myRequestType = ''
         # and indicate we are not expecting a response
@@ -59,6 +68,7 @@ process {
                 }
             }
         }
+
         # Walk over each parameter
         :nextParam foreach ($keyValue in $PSBoundParameters.GetEnumerator()) {
             # and walk over each of it's attributes to see if it part of the payload
@@ -92,12 +102,16 @@ process {
             # and optional data
             requestData = $paramCopy
         }
+
         if ($PassThru) {
             [PSCustomObject]$requestPayload
         } else {
             [PSCustomObject]$requestPayload | 
                 Send-OBS -NoResponse:$NoResponse
         }
+
 }
+
+
 } 
 

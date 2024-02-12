f967ea0d9a14d1a60541875946e43ebc9cc00bf0
James Brundage <@github.com>
2024-02-12 12:36:11 -0800

    feat: obs-powershell now mounts itself ( Fixes #180 )

--- obs-powershell.ps.psm1
+++ obs-powershell.ps.psm1
@@ -6,6 +6,14 @@ $MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
     Disconnect-OBS
 }
 
+$myModule = $MyInvocation.MyCommand.ScriptBlock.Module
+$NewDriveSplat = @{PSProvider='FileSystem';ErrorAction='Ignore';Scope='Global'}
+New-PSDrive -Name $myModule.Name -Root $PSScriptRoot -Description "$MyModule" @NewDriveSplat
+
+if ($home) {
+    New-PSDrive -Name "my-$($MyModule.Name)" -Root (Join-Path $home $myModule.Name) -Description "My $MyModule" @NewDriveSplat
+}
+
 foreach ($noun in 'Streaming','Recording') {
     foreach ($verb in 'Start', 'Stop') {
         Set-Alias "$verb-$noun" "$verb-OBS$($noun -replace 'ing')"        

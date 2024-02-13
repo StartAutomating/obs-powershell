d6757e977e34d1e7ab04cbf8e05836f2a418521c
StartAutomating <StartAutomating@github.com>
2024-02-13 19:25:19 +0000

    feat: Generating Shaders ( Fixes #163 )
        Fixing Internal Shader Names and Color Parameters

--- Commands/Filters/Shaders/Get-OBSSelectiveColorShader.ps1
+++ Commands/Filters/Shaders/Get-OBSSelectiveColorShader.ps1
@@ -79,9 +79,9 @@ $NoResponse
 
 
 process {
-if (-not $psBoundParameters['ShaderText']) {
-    $shaderName = $shaderName    
-    $ShaderNoun = 'OBSSelectiveColorShader'
+$shaderName = 'selective_color'
+$ShaderNoun = 'OBSSelectiveColorShader'
+if (-not $psBoundParameters['ShaderText']) {    
     $psBoundParameters['ShaderText'] = $ShaderText = '
 // Selective Color shader by Charles Fettinger for obs-shaderfilter plugin 3/2019
 //https://github.com/Oncorporation/obs-shaderfilter
@@ -215,7 +215,7 @@ switch -regex ($myVerb) {
                 [Regex]::Escape($FilterName)
             }
             else {
-                [Regex]::Escape($shaderName),[Regex]::Escape($ShaderNoun -replace '^OBS' -replace 'Shader$') -join '|'
+                [Regex]::Escape($ShaderNoun -replace '^OBS' -replace 'Shader$'),[Regex]::Escape($shaderName) -join '|'
             }
         ))"
         if ($SourceName) {

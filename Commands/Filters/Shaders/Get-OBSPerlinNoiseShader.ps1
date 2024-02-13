917bec1ad1f89f7f31d3129ec66b78e541572d4d
StartAutomating <StartAutomating@github.com>
2024-02-13 19:25:18 +0000

    feat: Generating Shaders ( Fixes #163 )
        Fixing Internal Shader Names and Color Parameters

--- Commands/Filters/Shaders/Get-OBSPerlinNoiseShader.ps1
+++ Commands/Filters/Shaders/Get-OBSPerlinNoiseShader.ps1
@@ -74,9 +74,9 @@ $NoResponse
 
 
 process {
-if (-not $psBoundParameters['ShaderText']) {
-    $shaderName = $shaderName    
-    $ShaderNoun = 'OBSPerlinNoiseShader'
+$shaderName = 'perlin_noise'
+$ShaderNoun = 'OBSPerlinNoiseShader'
+if (-not $psBoundParameters['ShaderText']) {    
     $psBoundParameters['ShaderText'] = $ShaderText = '
 //
 // Noise Shader Library for Unity - https://github.com/keijiro/NoiseShader
@@ -339,7 +339,7 @@ switch -regex ($myVerb) {
                 [Regex]::Escape($FilterName)
             }
             else {
-                [Regex]::Escape($shaderName),[Regex]::Escape($ShaderNoun -replace '^OBS' -replace 'Shader$') -join '|'
+                [Regex]::Escape($ShaderNoun -replace '^OBS' -replace 'Shader$'),[Regex]::Escape($shaderName) -join '|'
             }
         ))"
         if ($SourceName) {

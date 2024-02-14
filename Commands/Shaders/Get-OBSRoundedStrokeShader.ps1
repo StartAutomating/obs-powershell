da657bd439e419c127644f716c220ca0a3fecfb7
StartAutomating <StartAutomating@github.com>
2024-02-14 20:37:02 +0000

    feat: Shader Commands now support -Force ( Fixes #163 )
        Also fixing Set-OBSShaderFilter -ShaderFile

--- Commands/Shaders/Get-OBSRoundedStrokeShader.ps1
+++ Commands/Shaders/Get-OBSRoundedStrokeShader.ps1
@@ -39,6 +39,9 @@ $FilterName,
 [Alias('ShaderContent')]
 [String]
 $ShaderText,
+# If set, will force the recreation of a shader that already exists
+[Management.Automation.SwitchParameter]
+$Force,
 # If set, will pass thru the commands that would be sent to OBS (these can be sent at any time with Send-OBS)
 [Management.Automation.SwitchParameter]
 $PassThru,
@@ -238,7 +241,7 @@ switch -regex ($myVerb) {
             ShaderSetting = $ShaderSettings
             FilterName = $FilterName
             SourceName = $SourceName
-        }
+        }        
 
         foreach ($CarryOnParameter in "PassThru", "NoResponse","Force") {
             if ($PSBoundParameters.ContainsKey($CarryOnParameter)) {
@@ -260,7 +263,7 @@ switch -regex ($myVerb) {
             $ShaderFilterSplat.ShaderFile = $script:CachedShaderFilesFromCommand[$shaderName].FullName
         } else {
             $ShaderFilterSplat.ShaderText = $shaderText
-        }
+        }        
 
         if ($myVerb -eq 'Add') {                        
             Add-OBSShaderFilter @ShaderFilterSplat

ec1d47515b764f8cbc3fce17c9d7da52ba1470db
James Brundage <@github.com>
2024-02-14 16:30:59 -0800

    feat: Adding Reflect Shader ( Fixes #197 )
    Fixing shader find filter

--- Build/obs--shaders.build.ps1
+++ Build/obs--shaders.build.ps1
@@ -78,7 +78,7 @@ $CloneAndGetShaders = {
         Push-Location ($pwd | Split-Path)
         # Get shaders anywhere in the root repo (this will include the sparsely checked out repo)
         Get-ChildItem -Recurse -File | 
-            Where-Object -Extension -in '.shader','.effect' | 
+            Where-Object Extension -in '.shader','.effect' | 
             Where-Object { $_.Directory.Name -notin 'internal' }
         Pop-Location        
     )    

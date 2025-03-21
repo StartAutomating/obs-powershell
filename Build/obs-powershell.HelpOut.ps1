bb930676da9f540702d1cc0bcec15b90f49166f6
James Brundage <+@noreply.github.com>
2025-03-20 17:10:50 -0700

    fix: Generating MAML and allcommands.ps1 ( Fixes #214 )

--- Build/obs-powershell.HelpOut.ps1
+++ Build/obs-powershell.HelpOut.ps1
@@ -2,4 +2,5 @@
 Push-Location ($PSScriptRoot | Split-Path)
 Import-Module .\obs-powershell.psd1
 Save-MarkdownHelp -Module obs-powershell -PassThru
+Install-Maml -Module obs-powershell -PassThru -NoComment
 Pop-Location

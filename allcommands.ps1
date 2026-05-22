72c9d1ae387e1152519c14fdc665917fdd8aa85d
StartAutomating <StartAutomating@github.com>
2026-05-22 17:22:08 +0000

    feat(Stop-OBS): Adding Stop-OBS ( Fixes #226 )

--- allcommands.ps1
+++ allcommands.ps1
@@ -837,6 +837,112 @@ function Show-OBS {
     }
 } 
 #.ExternalHelp obs-powershell-Help.xml
+function Stop-OBS {
+    
+    [CmdletBinding(
+        ConfirmImpact='High',
+        PositionalBinding=$false,
+        SupportsShouldProcess
+    )]
+    param(
+    # If set, will stop recording.
+    [switch]
+    $Recording,
+
+    # If set, will stop streaming
+    [switch]
+    $Streaming,
+
+    # If set, will stop the virtual camera.
+    [switch]
+    $VirtualCamera,
+
+    # If set, will stop the OBS process.
+    [switch]
+    $Process,
+
+    # If set, will enable studio mode.
+    [switch]
+    $StudioMode
+    )
+
+    if ($Process) { 
+        $obsApp = $ExecutionContext.SessionState.InvokeCommand.GetCommand('obs', 'Application')
+        if (-not $obsApp -and -not ($IsMacOS -or $IsLinux)) {
+            $obsApp = $ExecutionContext.SessionState.InvokeCommand.GetCommand(
+                (
+                    Join-Path $env:ProgramFiles "obs-studio" |
+                        Join-Path -ChildPath 'bin' |
+                        Join-Path -ChildPath '64bit' |
+                        Join-Path -ChildPath 'obs64.exe'
+                ),
+                'Application'
+            )        
+        }
+
+        if (-not $obsApp) {
+            Write-Error "OBS not found"
+            return 
+        }
+        
+        $obsRunning = Get-Process | 
+            Where-Object Path -EQ $obsApp.Source
+
+        if (-not $obsRunning) {
+            Write-Error "OBS not running"
+            return
+        }
+
+        if ($WhatIfPreference) { return $obsRunning }
+        if ($PSCmdlet.ShouldProcess('Stop OBS')) {
+            $obsRunning | Stop-Process -PassThru
+        }
+        return
+    }
+
+    # Without any parameters, we will stop 
+    if (-not $PSBoundParameters.Count) {
+        # recording 
+        $Recording = $true
+        # and streaming.
+        $Streaming = $true        
+    }
+
+
+
+    # If we want to stop recording,
+    if ($Recording -and $PSCmdlet.ShouldProcess('Stop Recording')) {
+        # `Stop-ObsRecord`.
+        Stop-OBSRecord -PassThru:$WhatIfPreference
+    }
+
+    # If we want to stop the virtual camera,
+    if ($VirtualCamera -and $PSCmdlet.ShouldProcess('Stop Virtual Camera')) {
+        # `Stop-OBSVirtualCamera`.
+        Stop-OBSVirtualCam -PassThru:$WhatIfPreference
+    }
+
+    # If we want to stop streaming,
+    if ($Streaming -and $PSCmdlet.ShouldProcess('Stop Streaming')) {
+        # `Stop-OBSStream`. 
+        Stop-OBSStream -PassThru:$WhatIfPreference
+    }
+
+    if ($StudioMode -and $PSCmdlet.ShouldProcess('Stop Studio Mode')) {
+        Set-OBSStudioModeEnabled -StudioModeEnabled:$false -PassThru:$WhatIfPreference
+    }
+
+    # If we are shutting down any part of obs 
+    if ($Recording -or 
+        $Streaming -or 
+        $StudioMode -or 
+        $VirtualCamera) {
+        # we do not want to stop obs
+        return
+    }    
+}
+ 
+#.ExternalHelp obs-powershell-Help.xml
 function Watch-OBS
 {
     
